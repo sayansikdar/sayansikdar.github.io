ddaacb5dfde9243d30b7bd55909d2d39a3433504
Sayan Sikdar <sayan@fusioncharts.com>
2015-07-20 12:40:24 +0530

    all added hopefully

---
+++ lessons/weekend/project/project.js
@@ -0,0 +1,8 @@
+function jump()
+{
+	var modal1=document.getElementById('hiddenmodal');
+	var shownbox=document.getElementById('hiddendialog');
+
+modal1.style.visibility=(modal1.style.visibility=="visible")?"hidden":"visible";
+
+}
